a3f7e090f4f1a6a9aaf9595d865562573e6a0213
pc1 <pc120020202@gmail.com>
2026-06-02 21:53:27 +0800

    111 	new file:   fishros 	modified:   src/action/at_r2_bt/behavior_trees/grasp_head.xml 	modified:   src/action/at_r2_nav_bringup/config/nav2_params_mppi.yaml 	modified:   src/action/at_r2_nav_bringup/launch/at_navigation_launch.py 	modified:   src/location/point_lio/PCD/scans.pcd 	modified:   src/location/point_lio/config/mid360.yaml 	new file:   src/location/small_gicp 	modified:   src/location/small_gicp_relocalization/package.xml 	modified:   src/robot/at_r2_robot_description/resource/xmacro/AT_R2.sdf.xmacro

--- src/action/at_r2_nav_bringup/launch/at_navigation_launch.py
+++ src/action/at_r2_nav_bringup/launch/at_navigation_launch.py
@@ -61,7 +61,7 @@ def generate_launch_description():
 
     declare_world_cmd = DeclareLaunchArgument(
         "world",
-        default_value="rc_2026_blue",
+        default_value="rc_2026_red",
         description="Select world: 'rmul_2024' or 'rmuc_2024' (map file share the same name as the this parameter)",
     )
 
