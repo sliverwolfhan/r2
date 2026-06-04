21e55a9dcbe7c06c81c4613a9917470a9bffc70f
pc1 <pc120020202@gmail.com>
2026-06-04 14:13:35 +0800

    更改新的地图 	modified:   src/action/at_r2_bt/behavior_trees/grasp_head.xml 	modified:   src/action/at_r2_nav_bringup/config/nav2_params_mppi.yaml 	modified:   src/action/at_r2_nav_bringup/launch/at_navigation_launch.py 	modified:   src/action/at_r2_nav_bringup/launch/rviz_launch.py 	renamed:    src/action/at_r2_nav_bringup/map/rc_2026_red.pgm -> src/action/at_r2_nav_bringup/map/rc_2026_red3.pgm 	renamed:    src/action/at_r2_nav_bringup/map/rc_2026_red.yaml -> src/action/at_r2_nav_bringup/map/rc_2026_red3.yaml

--- src/action/at_r2_nav_bringup/launch/at_navigation_launch.py
+++ src/action/at_r2_nav_bringup/launch/at_navigation_launch.py
@@ -61,7 +61,7 @@ def generate_launch_description():
 
     declare_world_cmd = DeclareLaunchArgument(
         "world",
-        default_value="rc_2026_red",
+        default_value="rc_2026_red3",
         description="Select world: 'rmul_2024' or 'rmuc_2024' (map file share the same name as the this parameter)",
     )
 
